35a838c09f13e1838e01c57a2e195c23256b537b
kevinchang73 <kaichunchang@berkeley.edu>
2024-04-01 17:27:46 -0700

    ref #7 Visualization of trajectories

--- examples/multi_objective/run.sh
+++ examples/multi_objective/run.sh
@@ -1,2 +1,10 @@
-python multi.py -n 10 --headless -e multi_03 -sp multi_03/multi_03.scenic -gp multi_03/multi_03.graph -rp multi_03/multi_03_spec.py -s mab
-#python multi_03.py -n 2 --headless -e multi_03 -p multi_03/multi_03.scenic -s emab
+iteration=2
+rm multi_03/outputs/*.txt
+rm multi_03/outputs/*.png
+python multi_03/multi_03.py -n $iteration --headless -e multi_03 -sp multi_03/multi_03.scenic -gp multi_03/ -rp multi_03/multi_03_spec.py -s demab -o multi_03/outputs
+for i in $(seq 0 $(($iteration-1)));
+do
+    python util/plot_traj.py multi_03/outputs/multi_03_traj_$i.txt
+done
+#-m scenic.simulators.carla.model
+#python multi_03_old.py -n 2 --headless -e multi_03 -p multi_03/multi_03.scenic -s emab -m scenic.simulators.carla.model
