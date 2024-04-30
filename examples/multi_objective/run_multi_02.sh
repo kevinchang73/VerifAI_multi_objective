8eac758a3f155d783bc7777373eb434057f8c4d3
kevinchang73 <kaichunchang@berkeley.edu>
2024-04-30 15:58:30 -0700

    ref #7 Update scripts and include max simulation steps

---
+++ examples/multi_objective/run_multi_02.sh
@@ -0,0 +1,21 @@
+iteration=300
+scenario='multi_02'
+log_file="result_${scenario}.log"
+result_file="result_${scenario}.txt"
+sampler_order=0
+sampler_idx=0
+sampler_type=demab
+
+rm $scenario/outputs/*.txt
+rm $scenario/outputs/*.png
+rm $result_file
+for seed in $(seq 0 2);
+do
+    python $scenario/$scenario.py -n $iteration --headless -e $scenario -sp $scenario/$scenario.scenic -gp $scenario/ -rp $scenario/$scenario\_spec.py -s $sampler_type -o $scenario/outputs --seed $seed --using-sampler $sampler_idx > $log_file
+    for i in $(seq 0 $(($iteration-1)));
+    do
+        python $scenario/util/$scenario\_plot_traj.py $scenario/outputs/$scenario\_traj_$i.txt
+    done
+    python $scenario/util/$scenario\_collect_result.py $log_file multi $sampler_order >> $result_file
+done
+#-m scenic.simulators.carla.model --max-simulation-steps 600
