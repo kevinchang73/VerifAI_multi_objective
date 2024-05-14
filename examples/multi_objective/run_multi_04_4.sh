c45352924bb0aa4048214b49cc8bea1c7d59da12
kevinchang <kaichunchang@berkeley.edu>
2024-05-14 12:17:07 -0700

    ref #7 Add scripts for multi_04 and multi_05

---
+++ examples/multi_objective/run_multi_04_4.sh
@@ -0,0 +1,31 @@
+iteration=300
+scenario='multi_04'
+log_file="result_${scenario}_halton.log"
+result_file="result_${scenario}_halton.txt"
+csv_file="result_${scenario}_halton"
+sampler_idx=0 # 0 / 1 / 2 / -1 (-1 is for alternate)
+sampler_type=halton # demab / dmab / random / dce / halton / udemab
+simulator=scenic.simulators.carla.model
+to_plot=False # True / False
+simulation_steps=600
+
+rm $scenario/outputs/*traj*.txt
+rm $scenario/outputs/*traj*.png
+rm $scenario/outputs/$log_file
+rm $scenario/outputs/$result_file
+rm $scenario/outputs/$csv_file.csv
+rm $scenario/outputs/$csv_file\_scatter.png
+for seed in $(seq 0 2);
+do
+    if [[ $to_plot == 'True' ]]; then
+        python $scenario/$scenario.py -n $iteration --headless -e $csv_file.$seed -sp $scenario/$scenario.scenic -gp $scenario/ -rp $scenario/$scenario\_spec.py -s $sampler_type --seed $seed --using-sampler $sampler_idx -m $simulator --max-simulation-steps $simulation_steps -co $scenario/outputs -o $scenario/outputs >> $scenario/outputs/$log_file
+        for i in $(seq 0 $(($iteration-1)));
+        do
+            python $scenario/util/$scenario\_plot_traj.py $scenario/outputs/$scenario\_traj_$i.txt
+        done
+    else
+        python $scenario/$scenario.py -n $iteration --headless -e $csv_file.$seed -sp $scenario/$scenario.scenic -gp $scenario/ -rp $scenario/$scenario\_spec.py -s $sampler_type --seed $seed --using-sampler $sampler_idx -m $simulator --max-simulation-steps $simulation_steps -co $scenario/outputs >> $scenario/outputs/$log_file
+    fi
+done
+python $scenario/util/$scenario\_collect_result.py $scenario/outputs/$log_file multi $sampler_idx >> $scenario/outputs/$result_file
+python $scenario/util/$scenario\_analyze_diversity.py $scenario/outputs/ $csv_file multi >> $scenario/outputs/$result_file
--- examples/multi_objective/run_multi_04_single.sh
+++ examples/multi_objective/run_multi_04_single.sh
@@ -1,20 +1,31 @@
 iteration=300
 scenario='multi_04'
-log_file="result_${scenario}.log"
-result_file="result_${scenario}.txt"
-sampler_order=0
-sampler_idx=0
-sampler_type=demab
+log_file="result_${scenario}_single.log"
+result_file="result_${scenario}_single.txt"
+csv_file="result_${scenario}_single"
+sampler_idx=0 #redundant for single graph
+sampler_type=demab # demab / dmab / random / dce / halton / udemab
+simulator=scenic.simulators.carla.model
+to_plot=False # True / False
+simulation_steps=600
 
-rm $scenario/outputs/*.txt
-rm $scenario/outputs/*.png
-rm $result_file
-for seed in $(seq 0 2);
+rm $scenario/outputs/*traj*.txt
+rm $scenario/outputs/*traj*.png
+rm $scenario/outputs/$log_file
+rm $scenario/outputs/$result_file
+rm $scenario/outputs/$csv_file.csv
+rm $scenario/outputs/$csv_file\_scatter.png
+for seed in $(seq 0 3);
 do
-    python $scenario/$scenario.py -n $iteration --headless -e $scenario -sp $scenario/$scenario.scenic -gp $scenario/ -rp $scenario/$scenario\_spec.py -s $sampler -o $scenario/outputs --seed $seed --using-sampler $sampler_idx -m scenic.simulators.carla.model --max-simulation-steps 600 --single-graph > $log_file
-    for i in $(seq 0 $(($iteration-1)));
-    do
-        python $scenario/util/$scenario\_plot_traj.py $scenario/outputs/$scenario\_traj_$i.txt
-    done
-    python $scenario/util/$scenario\_collect_result.py $log_file multi $sampler_order >> $result_file
+    if [[ $to_plot == 'True' ]]; then
+        python $scenario/$scenario.py -n $iteration --headless -e $csv_file.$seed -sp $scenario/$scenario.scenic -gp $scenario/$scenario.sgraph -rp $scenario/$scenario\_spec.py -s $sampler_type --seed $seed --using-sampler $sampler_idx -m $simulator --max-simulation-steps $simulation_steps -co $scenario/outputs -o $scenario/outputs --single-graph >> $scenario/outputs/$log_file
+        for i in $(seq 0 $(($iteration-1)));
+        do
+            python $scenario/util/$scenario\_plot_traj.py $scenario/outputs/$scenario\_traj_$i.txt
+        done
+    else
+        python $scenario/$scenario.py -n $iteration --headless -e $csv_file.$seed -sp $scenario/$scenario.scenic -gp $scenario/$scenario.sgraph -rp $scenario/$scenario\_spec.py -s $sampler_type --seed $seed --using-sampler $sampler_idx -m $simulator --max-simulation-steps $simulation_steps -co $scenario/outputs --single-graph >> $scenario/outputs/$log_file
+    fi
 done
+python $scenario/util/$scenario\_collect_result.py $scenario/outputs/$log_file single $sampler_idx >> $scenario/outputs/$result_file
+python $scenario/util/$scenario\_analyze_diversity.py $scenario/outputs/ $csv_file single >> $scenario/outputs/$result_file
